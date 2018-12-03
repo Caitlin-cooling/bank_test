89483648f28c763f20c3bad1cd9ebf4512b4fb46
Caitlin Cooling <ccooling97@gmail.com>
2018-12-03 12:34:02 +0000

    Adjust stubbing of Time
    - Now method returns an instance of time so that it can be formatted 
    correctly

--- spec/features/view_account_info_spec.rb
+++ spec/features/view_account_info_spec.rb
@@ -2,14 +2,18 @@ require 'account'
 
 describe 'Check account information' do
   let(:account) { Account.new }
-  let(:date1) { allow(Time).to receive(:now).and_return '10-01-2012'}
+
+  before do
+    fake_time = Time.new(2012, 0o1, 10)
+    allow(Time).to receive(:now).and_return(fake_time)
+  end
 
   it 'shows empty date, credit, debit, balance' do
     expect(account.show_history).to include 'Date || Credit || Debit || Balance'
   end
 
   it 'shows that a user deposited money' do
-    account.desposit(1000.00, date1)
-    expect(account.show_history).to include '10-01-2012 || 1000.00 || || 1000.00'
+    account.desposit(1000.00)
+    expect(account.show_history).to include '10/01/2012 || 1000.00 || || 1000.00'
   end
 end
--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -2,7 +2,11 @@ require 'account'
 
 describe 'Account' do
   let(:account) { Account.new }
-  let(:date1) { allow(Time).to receive(:now).and_return '10-01-2012'}
+
+  before do
+    fake_time = Time.new(2012, 0o1, 10)
+    allow(Time).to receive(:now).and_return(fake_time)
+  end
 
   describe '#history' do
     it 'begins with headers' do
@@ -12,8 +16,8 @@ describe 'Account' do
 
   describe '#desposit' do
     it 'adds money to the users account' do
-      account.desposit(1000.00, date1)
-      expect(account.history).to eq ['Date || Credit || Debit || Balance', '10-01-2012 || 1000.00 || || 1000.00']
+      account.desposit(1000.00)
+      expect(account.history).to eq ['Date || Credit || Debit || Balance', '10/01/2012 || 1000.00 || || 1000.00']
     end
   end
 end
